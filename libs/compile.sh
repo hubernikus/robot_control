15035e603901424f5cb266066e640536ad62cba4
Lukas Huber <hubernikus@gmail.com>
2023-03-21 22:12:25 +0100

    Minor update for static binding.

--- libs/compile.sh
+++ libs/compile.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-g++ -o PinocchioWrapper.o -c PinocchioWrapper.cc -o overview-simple $(pkg-config --cflags --libs pinocchio) PinocchioWrapper.a
+
+# Convert library code to Object file
+g++ -o PinocchioWrapper.o -c PinocchioWrapper.cc $(pkg-config --cflags --libs pinocchio)
+
+# Create archive file/ static library
+ar rcs PinocchioWrapper.a PinocchioWrapper.o
